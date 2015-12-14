b064d0176b2409059432adff9edb9d0df60ec2c4
Morten Granlund <morten.granlund@gmail.com>
2015-12-14 22:04:16 +0000

    Added three test presentations, and added a .gitignore file

--- scripts/infograsper.sh
+++ scripts/infograsper.sh
@@ -7,22 +7,17 @@ pwd
 . infograsper.properties
 echo "DEBUG: Document root dir = $doc_root_dir"
 
-outdir="$doc_root_dir/$output_dir"
-
-## If the output dir does not exist, create it:
-[ -d "$outdir" ] || mkdir --verbose $outdir
-
 ## Convert from *.pptx or *.ppt to pps (automatic slide show)
-echo soffice --invisible --convert-to pps --outdir ${outdir} ${doc_root_dir}/*.ppt?
+soffice --invisible --convert-to pps --outdir ${output_dir} ${doc_root_dir}/*.ppt?
 
-echo "DEBUG: $(ls ${outdir})"
+echo "DEBUG: $(ls ${output_dir})"
 
-## Loop through 
-for nextFile in $(ls ${outdir})
+## Loop through all files in the presentation directory
+for nextFile in $(ls ${output_dir})
 do
    ## Present next file (slideshow) in the output dir:
    echo "Presenting ${nextFile}..."
-   soffice --show --nologo --norestore ${outdir}/${nextFile} &
+   soffice --show --nologo --norestore ${output_dir}/${nextFile} &
 
    ## Try to get the process ID (PID) of this presentation (we give libreoffice some seconds to get up and running)
    sleep 3s && presentationPID=$(pidof soffice.bin)
