79b1fdb50f0d7d8a64acfff8b77adc85c57414e7
Morten Granlund <morten.granlund@gmail.com>
2015-12-16 10:13:01 +0000

    Fixed a bug with the install script not having acces to the properties file before cloning the git repo

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,30 +5,27 @@ echo "INFO: Entering script $0"
 set -o errexit
 set -o nounset
 
-## 
-if [ "root" == "$(whoami)" ]
-then
+## Do not allow script to be run as root
+if [[ "$EUID" -eq 0 ]]; then
    echo "ERROR! You cannot be logged in as root!"
    exit 1
 fi
 
 ## Create git dir, if it does not already exist
-githome="~/git"
-[ -d "$githome" ] || mkdir "$githome"
+## NB! Cannot get the "magic" ~ to work as a user home substitute, so hardcoding the path to the "pi" user:
+githome="/home/pi/git"
+[ -d "$githome" ] || mkdir $githome
 
 ## Load latest version of application from git repo
 cd $githome
-git --git-dir=$githome clone $gitURL
-
-## Read properties from infograsper.properties
-. ~/git/InfoGrasper/scripts/infograsper.properties
+git --git-dir=$githome clone https://github.com/mogranlu/InfoGrasper.git
 
 ## Add some extra aliases
 echo -n ". $githome/InfoGrasper/scripts/my_aliases" >> ~/.bashrc
 
 ## Set correct locale on keyboard (input map)
-localectl set-keymap no106
-localectl set-x11-keymap no
+sudo localectl set-keymap no106
+sudo localectl set-x11-keymap no
 
 ## Update and upgrade OS to latest version
 sudo apt-get update
